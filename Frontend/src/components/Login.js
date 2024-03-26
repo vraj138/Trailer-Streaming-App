2bd21e56c4754da8dbdff0827610d5ea0cb46ac0
Vraj Patel <vcp37@scarletmail.rutgers.edu>
2024-03-26 17:35:43 -0400

    made few changes with login

--- Frontend/src/components/Login.js
+++ Frontend/src/components/Login.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react'
-import Header from './Header'
+import React, {useState} from 'react';
+import Header from './Header';
 import axios from "axios";
 import { API_END_POINT } from '../configs/constant';
 import toast from 'react-hot-toast';
